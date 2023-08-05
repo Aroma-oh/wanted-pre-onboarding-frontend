34359f3a1cd73a6d04bb7325e7d9d2d6ad61e5bd
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-05 20:09:54 +0900

    feat: 로그인 기능 구현

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,52 @@
+import { useInput } from '../hooks/useInput';
+import { AuthInput } from '../components/AuthInput';
+import { AuthButton } from '../components/AuthButton';
+import { emailValidate, passwordValidate } from '../utils/validation';
+import { signinAPI } from '../apis/signApi';
+
+import { useNavigate } from 'react-router';
+
 export const Signin = () => {
+  const [email, handleEmail] = useInput("");
+  const [password, handlePassword] = useInput("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    try {
+      await signinAPI(email, password);
+      navigate('/todo');
+    }
+    catch {
+      alert("실패");
+    }
+  }
+
   return (
-    <div>Signin</div>
+    <div>
+      <h1>로그인</h1>
+      <AuthInput
+        id='eamil'
+        type='text'
+        testid='email-input'
+        placeholder='user@wanted.com'
+        value={email}
+        onChange={handleEmail}
+      />
+      <AuthInput
+        id='password'
+        type='password'
+        testid='password-input'
+        placeholder='********'
+        value={password}
+        onChange={handlePassword}
+      />
+      <AuthButton
+        type='로그인'
+        testid='signin-button'
+        disabled={!emailValidate(email) || !passwordValidate(password)}
+        onClick={handleSubmit}
+      />
+    </div>
   )
 }
