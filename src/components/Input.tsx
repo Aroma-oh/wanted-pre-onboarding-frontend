9035d031e5260ad0d66fcd42f394082dd1cebeec
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-06 01:20:58 +0900

    rename: 컴포넌트 확장성을 위해 이름 변경

---
+++ src/components/Input.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent } from 'react'
+
+interface Props {
+  testid: string,
+  id: string,
+  type: string,
+  placeholder: string,
+  value: string,
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void,
+}
+
+export const Input = ({ testid, id, type, placeholder, value, onChange }: Props) => {
+  return (
+    <div className='input'>
+      <label htmlFor={id} />
+      <input
+        id={id}
+        type={type}
+        data-testid={testid}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+
+  )
+}
