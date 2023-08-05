95dac3e51b0efb70c2034da30dadafdbb87cad30
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-06 01:18:38 +0900

    style: code formatting

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -1,20 +1,20 @@
 import { redirect } from "react-router-dom";
 
 export const redirectTodo = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('access_token');
 
   if (token !== null) {
-    return redirect("/todo");
+    return redirect('/todo');
   }
 
   return null;
 }
 
 export const redirectSignin = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('access_token');
 
   if (token === null) {
-    return redirect("/signin");
+    return redirect('/signin');
   }
 
   return null;
