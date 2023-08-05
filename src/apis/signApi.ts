010eb7f7c8a3ad50dffc05b5794ead7cb556888b
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-06 01:18:20 +0900

    feat: 로그인 성공시 로컬 스토리지에 토큰 추가

--- src/apis/signApi.ts
+++ src/apis/signApi.ts
@@ -4,10 +4,11 @@ import { signAxios } from './axios';
 export const signinAPI = async (email: string, password: string) => {
   try {
     const response = await signAxios.post('/auth/signin', { email, password });
+    localStorage.setItem("access_token", response.data.access_token);
     return response;
   }
   catch {
-    throw new Error('로그인에 실패했습니다.');
+    alert("로그인에 실패했습니다.");
   }
 }
 
@@ -17,6 +18,6 @@ export const signupAPI = async (email: string, password: string) => {
     return response;
   }
   catch {
-    throw new Error('회원가입에 실패했습니다.');
+    alert("회원가입에 실패했습니다.");
   }
 }
