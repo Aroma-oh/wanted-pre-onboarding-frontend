f43e55a10a4c5283f29e182a30d355dc8197cf95
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-06 13:20:22 +0900

    feat: Todo Item 컴포넌트 생성

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,86 @@
+// react import
+import { useState } from 'react'
+// custom hook, api import 
+import { useInput } from '../hooks/useInput';
+import { deleteTodo, updateTodo } from '../apis/todoApi';
+// component import 
+import { Button } from './Button';
+import { Input } from './Input';
+
+interface Props {
+  todo: string;
+  id: number;
+  isCompleted: boolean;
+  fetchData: () => Promise<void>;
+}
+export const TodoItem = ({ todo, id, isCompleted, fetchData }: Props) => {
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const [todoModify, handleTodoModify, setTodoModify] = useInput('');
+
+  const handleModify = () => {
+    setIsModifyMode(true);
+    setTodoModify(todo);
+  };
+
+  const handleDelete = async (id: number) => {
+    await deleteTodo(id);
+    fetchData();
+  };
+
+  const handleSubmit = async (id: number) => {
+    await updateTodo(id, { todo: todoModify, isCompleted });
+    await fetchData();
+    setIsModifyMode(false);
+  }
+  const handleCancel = async () => {
+    setIsModifyMode(false);
+  };
+
+  return (
+    <>
+      {isModifyMode
+        ? (
+          <div>
+            <Input
+              testid='modify-input'
+              id='todo'
+              type='text'
+              value={todoModify}
+              onChange={handleTodoModify}
+            />
+            <div className='button-group'>
+              <Button
+                type='제출'
+                testid='submit-button'
+                onClick={() => handleSubmit(id)}
+              />
+              <Button
+                type='취소'
+                testid='cancel-button'
+                onClick={handleCancel}
+              />
+            </div>
+          </div>
+        )
+        : (
+          <div>
+            <span>{todo}</span>
+            <div className='button-group'>
+              <Button
+                type='수정'
+                testid='modify-button'
+                onClick={handleModify}
+              />
+              <Button
+                type='삭제'
+                testid='delete-button'
+                onClick={() => handleDelete(id)}
+              />
+            </div>
+          </div>
+        )
+      }
+    </>
+
+  )
+}
